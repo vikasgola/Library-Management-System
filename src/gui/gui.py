e69b4117d963417fc30427b470e9e1b75287550f
Vikas Gola <vikasgola2015@gmail.com>
2019-02-23 01:15:23 +0530

    delete data button completed

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -39,31 +39,22 @@ class App(QMainWindow):
 
 class SearchedWindow(QMainWindow):
     def __init__(self, parent, searchtext):
-        super(SearchedWindow, self).__init__(parent)
+        super(QMainWindow, self).__init__(parent)
         self.setWindowTitle("Search Output")
         self.tabnames = ["User", "Book", "Author", "Paper", "Periodical", "Message", "Publisher", "History"]
         
         queries = []
         for i in range(len(self.tabnames)):
             collist = self.getColumnNameList(tablename=self.tabnames[i])
-            q = "SELECT * FROM "+self.tabnames[i]+" WHERE "
+            q = "SELECT * FROM "+self.tabnames[i]+" WHERE ("
             for j in range(len(collist)):
-                q += collist[j]+" = '"+searchtext+"'"
+                q += collist[j]+" LIKE '%"+searchtext+"%')"
                 if j+1 < len(collist):
-                    q += " OR "
+                    q += " OR ("
             queries.append(q) 
 
-        centralWidget = QWidget(self)
-
-        self.layout = QGridLayout(self)
         self.tabWidget = TabWidget(self, queries)
-        self.bottombarWidget = BottombarWidget(self)
-
-        self.layout.addWidget(self.tabWidget, 0 , 0)
-        self.layout.addWidget(self.bottombarWidget, 1 , 0)
-
-        centralWidget.setLayout(self.layout)
-        self.setCentralWidget(centralWidget)
+        self.setCentralWidget(self.tabWidget)
 
     @LibMS
     def getColumnNameList(self, cursor , tablename):
@@ -78,7 +69,7 @@ class SearchedWindow(QMainWindow):
 
 class QueryWindow(QMainWindow):
     def __init__(self, parent, query):
-        super(QueryWindow, self).__init__(parent)
+        super(QMainWindow, self).__init__(parent)
         self.setWindowTitle("Query Output")
         tableWidget = TableWidget(self, query=query)
         self.setCentralWidget(tableWidget)       
@@ -88,6 +79,7 @@ class TopbarWidget(QWidget):
         super(QWidget, self).__init__(parent)
         self.layout = QHBoxLayout(self)
         self.setFixedHeight(64)
+        self.papa = parent
 
         self.searchBar = QTextEdit()
         self.searchBar.setPlaceholderText("Write Your 'SELECT' Query!")
@@ -108,27 +100,40 @@ class TopbarWidget(QWidget):
         self.layout.addWidget(self.btn1)
         self.setLayout(self.layout)
 
+    def closeDialog(self):
+        self.dialog.close()
+        self.searchBar.setText("")
+
     def handleSeachBar(self):
         if( ("SELECT" in self.searchBar.toPlainText()) or ("select" in self.searchBar.toPlainText()) ):
-            queryWindow = QueryWindow(self.parent(), self.searchBar.toPlainText())
+            queryWindow = QueryWindow(self.papa, self.searchBar.toPlainText())
             queryWindow.show()
         else:
-            pass
+            self.dialog = QMessageBox(self)
+            self.dialog.setIcon(QMessageBox.Critical)
+            self.dialog.setText("Not SELECT query!. Only SELECT queries can be used. Please Try Again with SELECT query.")
+            self.dialog.setWindowTitle("Not SELECT query!")
+            self.dialog.setStandardButtons(QMessageBox.Close)
+            self.dialog.buttonClicked.connect(self.closeDialog)
+            self.dialog.show()
 
 class BottombarWidget(QWidget):
     def __init__(self, parent):
         super(QWidget, self).__init__(parent)
         self.layout = QHBoxLayout(self)
         self.addSearchBar(parent)
+        self.papa = parent
 
         self.btn1 = QPushButton(self)
         self.btn1.setText("Add Data")
         self.btn1.setFixedWidth(100)
+        self.btn1.clicked.connect(self.handleAddButton)
         self.layout.addWidget(self.btn1, 1, Qt.AlignRight)
         
         self.btn2 = QPushButton(self)
         self.btn2.setText("Delete Data")
         self.btn2.setFixedWidth(100)
+        self.btn2.clicked.connect(self.handleDeleteButton)
         self.layout.addWidget(self.btn2, 0, Qt.AlignRight)
 
         self.setLayout(self.layout)
@@ -154,9 +159,77 @@ class BottombarWidget(QWidget):
 
     def handleSeachBar(self):
         searchtext = self.searchBar.text()
-        searchedWindow = SearchedWindow(self.parent(), searchtext)
+        searchedWindow = SearchedWindow(self.papa, searchtext)
         searchedWindow.show()
 
+    def handleAddButton(self):
+        print("add")
+
+    @LibMS
+    def deleteRow(self, cursor, query):
+        cursor.execute(query)
+
+    @LibMS
+    def getColumnNameList(self, cursor , tablename):
+        cursor.execute("SHOW COLUMNS FROM "+tablename)
+        data = cursor.fetchall()
+        headlist = []
+        for i in range(len(data)):
+            headlist.append(data[i][0])
+        return headlist
+
+    def handleDeleteButton(self, cursor):
+        index = self.papa.tabWidget.currentIndex()
+        tabWidget = self.papa.tabWidget.widget(index).layout.itemAt(0).widget()
+        selectedRows = [idx.row() for idx in tabWidget.selectionModel().selectedRows()]
+        self.dialog = QMessageBox(self)
+        self.dialog.setIcon(QMessageBox.Warning)
+        if len(selectedRows) != 0:
+            self.dialog.setText("These Rows will be deleted "+','.join([str(f) for f in selectedRows])+ " from table '"+self.papa.tabnames[index]+"'"+" \nAre You Sure?")
+            self.dialog.setStandardButtons( QMessageBox.Yes | QMessageBox.No)
+        else:
+            self.dialog.setText("No Row was selected. Please select rows.")
+            self.dialog.setStandardButtons( QMessageBox.Ok)
+
+        self.dialog.setWindowTitle("Deletion")
+
+        retval = self.dialog.exec_()
+        if retval == QMessageBox.No or retval == QMessageBox.Ok:
+            self.dialog.close()
+            return
+        self.dialog.close()
+
+        try:
+            for i in range(len(selectedRows)):
+                q = "DELETE FROM "+self.papa.tabnames[index]+" WHERE"
+                headlist = self.getColumnNameList(tablename=self.papa.tabnames[index])
+                
+                for j in range(len(headlist)):
+                    if ("date" in headlist[j]) or ("time" in headlist[j]):
+                        continue
+
+                    if "id" in headlist[j]:
+                        q += " ("+headlist[j]+" = "+tabWidget.item(selectedRows[i],j).text()+" )"
+                    elif "'" not in tabWidget.item(selectedRows[i],j).text():
+                        q += " ("+headlist[j]+" = '"+tabWidget.item(selectedRows[i],j).text()+"' )"
+                    else:
+                        q += " ("+headlist[j]+' = "'+tabWidget.item(selectedRows[i],j).text()+'" )'
+
+                    q += " OR"
+                q = q[:-2]
+                self.deleteRow(query=q)
+        except:
+            self.dialog2 = QMessageBox(self)
+            self.dialog2.setIcon(QMessageBox.Critical)
+            self.dialog2.setText("Most Probably. Foreign Key Constraint have Failed!. Try Again!")
+            self.dialog2.setWindowTitle("Wrong Query!")
+            self.dialog2.setStandardButtons(QMessageBox.Close)
+            self.dialog2.buttonClicked.connect(self.closeDialog)
+            self.dialog2.show()
+        self.papa.tabWidget.refresh()
+
+    def closeDialog(self):
+        self.dialog2.close()
 
 class TabWidget(QTabWidget):
     def __init__(self, parent, queries=None):
@@ -175,7 +248,10 @@ class TabWidget(QTabWidget):
             self.tablist[i].setLayout(self.tablist[i].layout)
 
     def refresh(self):
-        pass
+        index = self.currentIndex()
+        widget = self.widget(index).layout.itemAt(0).widget()
+        widget.clear()
+        widget.fillData()
 
 
 class TableWidget(QtWidgets.QTableWidget):
@@ -184,6 +260,8 @@ class TableWidget(QtWidgets.QTableWidget):
         self.tablename = tablename
         self.setEditTriggers(QAbstractItemView.NoEditTriggers)
         self.horizontalHeader().setSortIndicatorShown(True)
+        self.words = []
+
         if query == None and tablename != None:
             self.fillData()
         elif query != None and tablename != None:
@@ -191,21 +269,33 @@ class TableWidget(QtWidgets.QTableWidget):
         elif query != None and tablename == None:
             self.fillData(query=query)
 
+    def closeDialog(self):
+        self.dialog.close()
+        self.parent().close()
     
     @LibMS
     def fillData(self, cursor, query=None):
-        if query == None:
-            cursor.execute("SELECT * FROM "+self.tablename)
-        else:
-            print(query)
-            cursor.execute(query)
+        try:
+            if query == None:
+                cursor.execute("SELECT * FROM "+self.tablename)
+            else:
+                cursor.execute(query)
+        except:
+            self.dialog = QMessageBox(self)
+            self.dialog.setIcon(QMessageBox.Critical)
+            self.dialog.setText("Wrong Query detected!. Please try again with correct query.")
+            self.dialog.setWindowTitle("Wrong Query!")
+            self.dialog.setStandardButtons(QMessageBox.Close)
+            self.dialog.buttonClicked.connect(self.closeDialog)
+            self.dialog.show()
+            return
+
         data = cursor.fetchall()
         
         if(len(data) != 0):
             self.setColumnCount(len(data[0]))
             self.setRowCount(len(data))
 
-            self.words = []
             for d in range(len(data)):
                 for dd in range(len(data[d])):
                     self.words.append(str(data[d][dd]))
